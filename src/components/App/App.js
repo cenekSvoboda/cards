9603e00b5bc19bd9a2342a8856d707bf80cebc4f
svobo <svobo.c@gmail.com>
2021-01-10 04:53:14 +0100

    uklid 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../App/App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Login from '../Login/Login';
 import Preferences from '../Preferences/Preferences.js';
 import useToken from '../App/useToken';
-
-function setToken(userToken) {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-    const tokenString = sessionStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token;
-}
 
 function App() {
     const { token, setToken } = useToken();
