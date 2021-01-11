23d7d25f3e65b885f19d8953df5554128bb184bc
svobo <svobo.c@gmail.com>
2021-01-11 11:16:55 +0100

    #CARDS-6 - Uzavřít login, layout a základní routing před branchem Cards (obecné řešení)
    async sqlite

--- server.tsx
+++ server.tsx
@@ -2,89 +2,103 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 var bodyParser = require('body-parser')
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('sqlite-async');
 
 
-app.use('/resetDB', (req, res) => {
+app.use('/resetDB', async (req, res) => {
 
-    let db = new sqlite3.Database('./cards.db', (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the cards database.');
-    });
+    try {
+        db = await Database.open('./cards.db');
+    } catch (E) {
+        console.log("Cant connect");
+    }
 
+    await db.run(`
+        CREATE TABLE IF NOT EXISTS contacts (
+        contact_id INTEGER PRIMARY KEY,
+        shorthand TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        token TEXT,
+        name TEXT NOT NULL,
+        phone TEXT,
+        optional1 TEXT,
+        optional2 TEXT,
+        url TEXT
+        );
+    `);
 
-    let sql = `
-    CREATE TABLE IF NOT EXISTS contacts (
-	contact_id INTEGER PRIMARY KEY,
-	shorthand TEXT NOT NULL UNIQUE,
-	email TEXT NOT NULL UNIQUE,
-	password TEXT NOT NULL,
-	token TEXT,
-	name TEXT NOT NULL,
-	phone TEXT,
-	optional1 TEXT,
-	optional2 TEXT,
-	url TEXT
-);
+    const delString = `
+           DELETE FROM contacts WHERE TRUE;
+        `;
+    await db.run(delString);
 
-`;
+    const insertString = `
+    INSERT INTO contacts (
+        shorthand,
+        email,
+        password,
+        token,
+        name,
+        phone,
+        optional1,
+        optional2,
+        url
+    )
+    VALUES(
+        'admin',
+        'svobo.c@gmail.com',
+        'admin',
+        ?,
+        'Admin Adminovic',
+        '666666666',
+        'Hell Lane 1',
+        'Hell',
+        'http://ceneksvoboda.eu'
+    );
+        `;
+    await db.run(insertString, '');
 
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            console.log(row.name);
-        });
-    });
-
-    db.close();
-
-
-    let db2 = new sqlite3.Database('./cards.db', (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the cards database.');
-    });
-    let sql2 = `
+    const insertString2 = `
     
-INSERT INTO contacts (
-	shorthand,
-	email,
-	password,
-	token,
-	name,
-	phone,
-	optional1,
-	optional2,
-	url
-)
-VALUES(
-'admin',
-'svobo.c@gmail.com',
-'admin',
-'',
-'Admin Adminovic',
-'666666666',
-'Hell Lane 1',
-'Hell',
-'http://ceneksvoboda.eu'
-);`;
+    INSERT INTO contacts (
+        shorthand,
+        email,
+        password,
+        token,
+        name,
+        phone,
+        optional1,
+        optional2,
+        url
+    )
+    VALUES(
+        'admasdfin',
+        'svoasfdbo.c@gmail.com',
+        'admfasdin',
+        'asdfdsfsdf',
+        'Adfsdmin Adminovic',
+        '6666d66666',
+        'Hesfll Lane 1',
+        'Hefsll',
+        'http://cenekssfvoboda.eu'
+    );
+        `;
+    await db.run(insertString2);
 
-    db2.all(sql2, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            console.log(row.name);
-        });
-    });
 
-    db2.close();
-    res.send("reset");
+    /*var x =
+        await db.get(`SELECT *
+                         FROM contacts WHERE shorthand LIKE ?`,
+            "%%");
+    console.log(x);*/
+
+    var x =
+        await db.all(`SELECT * 
+                         FROM contacts`);
+    console.log(x);
+    res.end(`<h1>hello ${JSON.stringify(x)}</h1>`);
+
 });
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
